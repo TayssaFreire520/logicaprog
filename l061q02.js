adc3bbfe0ec7c1510e3e3a67399a951703bdad5f
Tayssa Freire <tayssafreiremesquita@gmail.com>
2026-06-17 11:53:09 -0300

    segundo swich case

---
+++ l061q02.js
@@ -0,0 +1,20 @@
+// Desenvolver um programa que pergunte o sexo da pessoa 
+// e dê como resposta a seguinte orientação de acordo com o sexo informado: 
+// “Banheiro masculino à direita” ou “Banheiro feminino à esquerda”.
+
+console.log ("Iforme seu sexo: ");
+console.log ("Informe o código referente ao banheiro desejado: ");
+console.log ("2 - FEMININO");
+console.log ("3 - MASCULINO");
+cod = 2
+cod = 3
+
+    switch ( cod ) {
+case 2:
+console.log ("Você selecionou: 2 - FEMININO a esquerda");
+ break;
+
+ case 3:
+console.log ("Você selecionou: 3 - MASCULINO a direita");
+ break;
+    }

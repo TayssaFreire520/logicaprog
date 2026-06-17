1572ebb96999dd8cd769de730e0e78a6ac12fb5e
Tayssa Freire <tayssafreiremesquita@gmail.com>
2026-06-17 11:04:31 -0300

    Exemplo com else if

---
+++ l041q06.js
@@ -0,0 +1,16 @@
+//Desenvolver um programa que pergunte dois valores numéricos inteiros e 
+// apresente o valor da diferença entre o maior valor e o menor valor lido.
+
+let num1, num2, diferenca;
+
+console.log ("Informe o primeiro número:");
+num1 = 3;
+
+console.log ("Informe o segundo segundo número:");
+num2 = 9;
+
+diferenca = 3 - 9;
+
+if (diferenca = num1 - num2)
+
+console.log (`A diferença é de ${diferenca}`);
---
+++ l041q07.js
@@ -0,0 +1,7 @@
+//Desenvolver um programa que pergunte um valor inteiro positivo ou negativo, 
+//e exiba como resposta o módulo deste valor, ou seja, o número lido como sendo positivo.
+
+let num, positivo, negativo;
+console.log ("Informe o valor inteiro:");
+num = 7;
+console.log (`Número informado ${num}`);

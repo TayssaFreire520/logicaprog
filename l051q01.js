9b84e3fda539a505ce1aba530253c001e9081696
Tayssa Freire <tayssafreiremesquita@gmail.com>
2026-06-18 09:08:25 -0300

    Primeiro while

---
+++ l051q01.js
@@ -0,0 +1,8 @@
+//Desenvolver um programa que exiba os números de 1 a 100.
+let cont = 1; //Contador
+
+while ( cont <= 100) {
+
+    console.log( cont );
+    cont = cont +1; // Incremento do contador (faz sentido ser a ultima linha).
+}

60d6269091132a57d7e322eb98da41330493bde2
Tayssa Freire <tayssafreiremesquita@gmail.com>
2026-06-26 09:34:29 -0300

    alteracaoprova2

--- l071q02.js
+++ l071q02.js
@@ -1,16 +1,10 @@
 //Desenvolver um programa que pergunte cinco elementos de um vetor a. 
 // No final, apresente a soma de todos os elementos deste vetor.
 
-let num1 = 9;
-let num2 = 7;
-let num3 = 4;
-let num4 = 2;
-let num5 = 5;
-
-let lista = [num1, num2, num3, num4, num5];
+let a = [9, 7, 4, 2, 5];
 let acum = 0;
 
 for (let cont = 0; cont <= 4; cont++) {
- acum = acum + lista[cont]
+ acum = acum + a[cont]
 };
 console.log (`Soma: ${acum}`)
